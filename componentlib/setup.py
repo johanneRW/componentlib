abd7fa72f7682360ebc5b5cbfb930ea0ddee1987
Johanne <jrletsgo2012@gmail.com>
2025-06-04 12:41:05 +0200

    Rename folder

--- componentlib/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="componentlib",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=["Django>=3.2"],
-)
