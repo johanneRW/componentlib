6362c62681231a0156cf2297f5c770328f84b89a
Johanne <jrletsgo2012@gmail.com>
2025-06-10 16:19:11 +0200

    update dropdown

--- componentlib/components/dropdown/props.py
+++ componentlib/components/dropdown/props.py
@@ -4,11 +4,10 @@ from pydantic import BaseModel, Field
 from typing import List, Optional, Tuple
 
 class DropdownComponentProps(BaseModel):
-    name: str = Field("dropdown")
-    label: str = Field("Choose option")
-    options: List[Tuple[str, str]] = Field([])
-    target_url: str = Field("/htmx/dropdown/dropdown_result_view/")
-    placeholder: Optional[str] = Field(None)
-    value: Optional[str] = Field(None)
-    disabled: bool = Field(False)
-
+    name: str = Field(..., description="Name of the dropdown field")
+    label: str = Field(..., description="Label for the dropdown field")
+    options: List[Tuple[str, str]] = Field(default_factory=list, description="List of options for the dropdown")
+    target_url: str = Field(..., description="URL to fetch dropdown options from")
+    placeholder: Optional[str] = Field(None, description="Placeholder text for the dropdown")
+    value_from: Optional[str] = Field(None, description="Initial value for the dropdown")
+    disabled_from: bool = Field(False, description="Whether the dropdown is disabled")
